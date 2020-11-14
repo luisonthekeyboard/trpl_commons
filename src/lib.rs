8e828e1024af6d05b8df1839cf32f4f5d8f4778c
Luís Rodrigues Soares <luis@luisonthekeyboard.com>
2020-11-14 10:36:46 +0100

    Add a function to read and parse from the terminal.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use std::str::FromStr;
+use std::io;
+
+pub fn read_and_parse<T: FromStr>() -> Result<T, T::Err> {
+    let mut to_as_str = String::new();
+
+    io::stdin()
+        .read_line(&mut to_as_str)
+        .expect("Failed to read line");
+
+    return to_as_str.trim().parse();
 }
